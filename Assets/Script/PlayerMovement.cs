6f35ecf1e7a92eeb3cae02ae9df260a8f79ad807
Amanda <amanda.vanhatalo@tuni.fi>
2020-05-30 19:27:17 +0300

    Added RollABoll and PlayerMovement

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Starting Position: " + transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movementVector = new Vector3(0.05f, 0, 0);
+        transform.Translate(movementVector);
+
+        Debug.Log("Current Position: " + transform.position);
+    }
+}
